20a7e8189c56c44a101adc839dff4032d21b1c29
framista <adajab1@gmail.com>
2022-02-12 21:20:35 +0100

    Update tests

--- src/components/ButtonNavigation/ButtonNavigation.test..js
+++ src/components/ButtonNavigation/ButtonNavigation.test..js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import Settings from './Settings';
+import ButtonNavigation from './ButtonNavigation';
 
 describe('Layout', () => {
   let props;
@@ -17,19 +17,19 @@ describe('Layout', () => {
   });
 
   it('should be next button', () => {
-    render(<Settings {...props} />);
+    render(<ButtonNavigation {...props} />);
     const nextButton = screen.getByRole('button', { name: 'Next' });
     expect(nextButton).toBeInTheDocument();
   });
 
   it('should be prev button', () => {
-    render(<Settings {...props} />);
+    render(<ButtonNavigation {...props} />);
     const prevButton = screen.getByRole('button', { name: 'Prev' });
     expect(prevButton).toBeInTheDocument();
   });
 
   it('should be prev button initially disabled', () => {
-    render(<Settings {...props} />);
+    render(<ButtonNavigation {...props} />);
     const prevButton = screen.getByRole('button', { name: 'Prev' });
     expect(prevButton).toBeDisabled();
   });
@@ -49,14 +49,14 @@ describe('Interactions', () => {
   });
 
   it('should be called increment function after clicking on next button', () => {
-    render(<Settings {...props} />);
+    render(<ButtonNavigation {...props} />);
     const nextButton = screen.getByRole('button', { name: 'Next' });
     userEvent.click(nextButton);
     expect(props.handlersForId.increment.mock.calls.length).toBe(1);
   });
 
   it('should be called decrement function after clicking on prev button', () => {
-    render(<Settings {...props} id={2} />);
+    render(<ButtonNavigation {...props} id={2} />);
     const prevButton = screen.getByRole('button', { name: 'Prev' });
     userEvent.click(prevButton);
     expect(props.handlersForId.decrement.mock.calls.length).toBe(1);
