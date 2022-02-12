bdee939094d70706b3776b022418478bc993d2bf
framista <adajab1@gmail.com>
2022-02-12 18:18:57 +0100

    Refactor card skeleton and error

--- src/components/common/CardError/CardError.jsx
+++ src/components/common/CardError/CardError.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export const CardError = () => {
-  return (
-    <div className="w-[500px] max-w-[90%] bg-white rounded shadow-2xl">
-      <div className="h-[200px] bg-gray-200 rounded-tr rounded-tl flex justify-center items-center">
-        <p className="text-red-500 font-bold">Unexpected problem</p>
-      </div>
+export const CardError = () => (
+  <div className="w-[500px] max-w-[90%] bg-white rounded shadow-2xl">
+    <div className="h-[200px] bg-gray-200 rounded-tr rounded-tl flex justify-center items-center">
+      <p className="text-red-500 font-bold">Unexpected problem</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default React.memo(CardError);
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,1 +1,3 @@
 export { default as Button } from './Button/Button';
+export { default as CardSkeleton } from './CardSkeleton/CardSkeleton';
+export { default as CardError } from './CardError/CardError';
